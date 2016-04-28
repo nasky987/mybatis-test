063578526a7afd15f7edfe5ef05f40ea582b2b55
JOHN LEE <nasky987@gmail.com>
2016-04-28 21:27:55 +0900

    마이바티스 챕터1

---
+++ mybatis-test1/sql/STUDENTS.sql
@@ -0,0 +1,14 @@
+CREATE TABLE STUDENTS
+(
+	stud_id int(11) NOT NULL AUTO_INCREMENT,
+	name varchar(50) NOT NULL,
+	email varchar(50) NOT NULL,
+	dob date DEFAULT NULL,
+	PRIMARY KEY(stud_id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+insert into STUDENTS(stud_id, name, email, dob)
+values (1, 'Student1', 'student1@gmail.com', '1983-06-25');
+
+insert into STUDENTS(stud_id, name, email, dob)
+values (2, 'Student2', 'student2@gmail.com', '1983-06-25');
